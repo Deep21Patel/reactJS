7bd39d97e523df36a6fcc00dce280a6f911f8754
Deep21Patel <109586036+Deep21Patel@users.noreply.github.com>
2022-11-10 09:27:36 +0530

    ready

--- api/script.js
+++ api/script.js
@@ -1,30 +1,49 @@
-function regApi(){
 
-    let uname=document.getElementById('uname').value;
 
-    //console.log(uname);
-    let email=document.getElementById('email').value;
-    let password=document.getElementById('password').value;
-    let mobile=document.getElementById('mobile').value;
+//Login
+loginData = document.querySelector('#btn-login');
+userlocalstorage = localStorage.getItem('userId');
+console.log(userlocalstorage);
+if(loginData != null)
+{
+    loginData.addEventListener('click',function(){
+        let namevar = document.getElementById('name').value;
+        let pwdvar = document.getElementById('pwd').value;
+        fetch('http://localhost/api/login_get_method?username='+namevar+'&&password='+pwdvar).then(res=>res.json())
+        .then((Response)=>{
+            console.log(Response);
+            if(Response.Code == 1)
+            {
+                localStorage.setItem('userId',Response.Data.id);
+                window.location.href="dashbord.html";
+                console.log("Login Done");
+            }
+            else{
+                alert("Something Went Wrong");
+            }
+        })
+    })
+}
+//registeration
+function Reg_API()
+{
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let pwd = document.getElementById('pwd').value;
+    let mobile = document.getElementById('mobile').value;
+    const AllData = {username:name,email:email,password:pwd,mobile:mobile}
+    console.log(AllData);
 
-    const data={username:uname,email:email,password:password,mobile:mobile}
-    console.log(data);
-    fetch('',{method:'post',headers:{
-        'content-type':'application/json','accept':'application/json',
-
-    },body:JSON.stringify({allData:data})}).then(res=>res.json()).then(Responce=>{
-        console.log(Responce);
-        
-
-        if(Responce.code==1)
+    fetch('http://localhost/api/registration_api',{method:'POST',headers:{
+        'Content-Type':'application/json','Accept':'application/json'
+    },body:JSON.stringify({allData:AllData})}).then(res=>res.json()).then((Response)=>{
+        console.log(Response);
+        if(Response.Code == 1)
         {
-            localStorage.setItem('userid',Responce.data)
-
+            localStorage.setItem('userId',Response.Data);
         }
         else{
-            console.log("somthing want wrong");
+            console.log("Something Went Wrong");
         }
     })
-
-
 }
